fc56ae58675cfe755757f9f2a0adba6cd214a262
Michiel Leunens <michiel.leunens@gmail.com>
2022-05-22 23:13:06 +0200

    Adjust keyframe menu

--- components/common/Navbar/NavItems.tsx
+++ components/common/Navbar/NavItems.tsx
@@ -19,9 +19,9 @@ const NavItems: VFC<NavItemsProps> = ({
     <AnimatePresence>
       {navDrawerOpen && (
         <motion.div
-          initial={{ opacity: 0, scale: 0.9 }}
-          animate={{ opacity: 1, scale: 1 }}
-          exit={{ opacity: 0, scale: 0.9 }}
+          initial={{ opacity: 0, x: 100 }}
+          animate={{ opacity: 1, x: 0 }}
+          exit={{ opacity: 0, x: 100 }}
           transition={{ duration: 0.33, type: 'spring' }}
           className={cn(styles.navMenuContainer, {
             [styles.navigationTransparent]: isTransparent,
